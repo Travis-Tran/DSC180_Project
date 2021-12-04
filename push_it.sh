81aac473ae8a2cf91793597c23ff78b8ee623741
hiroki-mtg <cheeto.with.a.stack@gmail.com>
2021-12-04 02:29:12 -0800

    $commitMessage

--- push_it.sh
+++ push_it.sh
@@ -2,12 +2,8 @@ git add .
 
 echo 'Enter the commit message:'
 read commitMessage
-
 git commit -m'$commitMessage'
 
-echo 'Enter the name of the branch:'
-read branch
-
-git push $branch
+git push
 
 read
